5b151d34d4985c5b9bfe2711b2e4aada1a247c0f
PranavPurwar <purwarpranav80@gmail.com>
2021-12-02 22:25:38 +0530

    update snackbar logic

--- app/src/main/java/my/first/as/kotlinapp/MainActivity.kt
+++ app/src/main/java/my/first/as/kotlinapp/MainActivity.kt
@@ -2,14 +2,13 @@ package my.first.`as`.kotlinapp
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import my.first.`as`.kotlinapp.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textView: TextView
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +17,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         Log.v("tag", "Verbose")
         binding.button.setOnClickListener {
-            Snackbar.make(this, binding.root, "Snackbar text", Snackbar.LENGTH_SHORT).show()
+            Snackbar.make(this, binding.root, "Snackbar text", Snackbar.LENGTH_SHORT)
+                .setAction("Button", {
+                    Toast.makeText(this, "Clicked", Toast.LENGTH_LONG).show()
+                }
+                )
+                .show()
         }
     }
 }
