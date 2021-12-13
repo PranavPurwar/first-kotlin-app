2ad89228517e481913974dd446e7a9c4532bde60
PranavPurwar <purwarpranav80@gmail.com>
2021-12-13 11:53:28 +0530

    fix some compile errors

--- app/src/main/kotlin/my/first/as/kotlinapp/ide/DexTask.kt
+++ app/src/main/kotlin/my/first/as/kotlinapp/ide/DexTask.kt
@@ -3,22 +3,23 @@ package my.first.`as`.kotlinapp.ide
 import com.pranav.ide.dx.command.dexer.Main
 
 import java.io.File
+import java.util.List
 import java.lang.reflect.Method
 
 class DexTask {
 
 fun doFullTask() {
         val f = File("/storage/emulated/0/classesTest.jar")
-		List<String> args = Arrays.asList(
+		val args: List<String> = Arrays.asList(
 		"--debug",
 		"--verbose",
 		"--min-sdk-version=21",
 		"--output=" + f.getParent(),
-		input
+		f.getAbsolutePath()
 		)
 		Main.clearInternTables()
-		val arguments = Main.Arguments()
-		parseMethod: Method = Main.Arguments::class.getDeclaredMethod("parse", String[]::class)
+		val arguments: Main.Arguments = Main.Arguments()
+		parseMethod: Method = (Main.Arguments::class).getDeclaredMethod("parse", String::class[])
 		parseMethod.isAccessible = true
 		parseMethod.invoke(arguments, (Object) args.toArray(String[0]))
 		Main.run(arguments)
