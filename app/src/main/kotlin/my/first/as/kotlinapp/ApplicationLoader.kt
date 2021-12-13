f1282db6357530f7d288a08d1d41e519b43075f7
PranavPurwar <purwarpranav80@gmail.com>
2021-12-13 19:00:39 +0530

    fix some code

--- app/src/main/kotlin/my/first/as/kotlinapp/ApplicationLoader.kt
+++ app/src/main/kotlin/my/first/as/kotlinapp/ApplicationLoader.kt
@@ -8,13 +8,13 @@ import android.widget.Toast
 class ApplicationLoader: Application() {
 
     companion object {
-        lateinit val context: ApplicationLoader
+        lateinit var context: ApplicationLoader
     }
 
     override fun onCreate() {
         super.onCreate()
         context = this
-        Thread.setDefaultUncaughtExceptionHandler(Thread.UncaughtExceptionHandler() {
+        Thread.setDefaultUncaughtExceptionHandler({
 			fun uncaughtException(thread: Thread, throwable: Throwable) {
                 Toast.makeText(this, Log.getStackTraceString(throwable), Toast.LENGTH_SHORT).show()
             }
