0885f4645ce899ee63c120628d2544898594af6f
PranavPurwar <purwarpranav80@gmail.com>
2021-12-13 18:35:26 +0530

    handler test 2

--- app/src/main/kotlin/my/first/as/kotlinapp/ApplicationLoader.kt
+++ app/src/main/kotlin/my/first/as/kotlinapp/ApplicationLoader.kt
@@ -7,7 +7,7 @@ import android.widget.Toast
 
 class ApplicationLoader: Application() {
 
-    var context: Context?
+    var context: Context
 
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
@@ -15,9 +15,12 @@ class ApplicationLoader: Application() {
     }
     
     override fun onCreate() {
-        Thread.setDefaultUncaughtExceptionHandler(thread, throwable -> {
-            Toast.makeText(context, Log.getStackTraceString(throwable), Toast.LENGTH_SHORT).show()
+        Thread.setDefaultUncaughtExceptionHandler(Thread.UncaughtExceptionHandler() {
+			override fun uncaughtException(Thread thread, Throwable throwable) {
+                Toast.makeText(context, Log.getStackTraceString(throwable), Toast.LENGTH_SHORT).show()
+            }
         })
+        super.onCreate()
     }
     
     fun getContext(): Context {
