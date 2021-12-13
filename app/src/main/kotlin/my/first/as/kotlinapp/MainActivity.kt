f0e245474b4512059d0f945f6d05ca16560e1b2d
PranavPurwar <purwarpranav80@gmail.com>
2021-12-13 15:35:24 +0530

    !

--- app/src/main/kotlin/my/first/as/kotlinapp/MainActivity.kt
+++ app/src/main/kotlin/my/first/as/kotlinapp/MainActivity.kt
@@ -16,23 +16,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val crashButton = Button(this)
+        crashButton.text = "Test Crash"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+       addContentView(crashButton, ViewGroup.LayoutParams(
+           ViewGroup.LayoutParams.MATCH_PARENT,
+           ViewGroup.LayoutParams.WRAP_CONTENT))
+/*        setContentView(binding.root)
         Log.v("tag", "Verbose")
         binding.button.setOnClickListener {
             Snackbar.make(this, binding.root, "Run dx", Snackbar.LENGTH_SHORT)
                 .setAction("Button", {
                     Toast.makeText(this, "Starting", Snackbar.LENGTH_LONG).show()
                     throw RuntimeException("Test Crash")
-//                    val tasks: Array<Task> = arrayOf(
-//                        val task: DexTask = DexTask("/storage/emulated/0/classesTest.jar")
-//                        task.doFullTask();
-//                    )
-//                    for (task in tasks) {
-//                        Toast.makeText(this, "Task name: " + task.getTaskName(), Toast.LENGTH_SHORT).show()
-//                        task.doFullTask();
-//                    }
+                    val tasks: Array<Task> = arrayOf(
+                        val task: DexTask = DexTask("/storage/emulated/0/classesTest.jar")
+                        task.doFullTask();
+                    )
+                    for (task in tasks) {
+                        Toast.makeText(this, "Task name: " + task.getTaskName(), Toast.LENGTH_SHORT).show()
+                        task.doFullTask();
+                    }
                 }).show()
                 
-        }
+*/        }
     }
 }
